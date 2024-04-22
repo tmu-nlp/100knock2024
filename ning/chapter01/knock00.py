7ca65e4e2871d6179f9702215250a6bee3ddaaa9
d36n <daining36@gmail.com>
2024-04-22 16:14:52 +0900

    message

--- ning/chapter01/knock00.py
+++ ning/chapter01/knock00.py
@@ -3,7 +3,7 @@ s = "stressed"
 srl = list(reversed(s))
 #reversedはlistしか使えないため、文字列をリスト化する
 
-sr = ''.j oin(list(reversed(s)))
+sr = ''.join(list(reversed(s)))
 #一文字ずつが要素として格納されたため、joinを使い一つの文字列に連結する
 
 print(s[::-1])
