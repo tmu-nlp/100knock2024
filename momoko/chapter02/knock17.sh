674717694c9a8b49540216ba5e4c97e6ebbe6446
moi_moisture <138105872+momok47@users.noreply.github.com>
2024-05-05 16:52:48 +0900

    Update knock17.sh

--- momoko/chapter02/knock17.sh
+++ momoko/chapter02/knock17.sh
@@ -2,4 +2,4 @@ cut -d " " -f 1 "/Users/shirakawamomoko/Desktop/nlp100保存/chapter02//popular-
 #-d：区切り文字について，タブの代わりに使用する文字を指定する．今回は" "．
 #-f 1：1列目を指定
 #sort：辞書順にデータを並び替え
-#uniq：重複データの削除(連続しているもの．sortで連続している．)
+#uniq：重複データの削除(連続しているもの．sortで並び替えしている．)
