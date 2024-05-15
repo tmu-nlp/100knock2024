853f14053413a7f9c3a1ec3e8d0217f30c797f26
Siqing Wang <167733437+etoiledumatin27@users.noreply.github.com>
2024-05-16 00:18:02 +0900

    Update knock16.py

--- siqingwang/chapter02/knock16.py
+++ siqingwang/chapter02/knock16.py
@@ -3,7 +3,7 @@
 import pandas as pd
 import numpy as np
 
-file_path = '/Users/hoshikawakiyoru/Library/Mobile Documents/com~apple~CloudDocs/ソーシャル・データサイエンス/NLP/popular-names.txt'
+file_path = '/Users/hoshikawakiyoru/Downloads/popular-names.txt'
 df = pd.read_csv(file_path, delimiter='\t', header=None)
 
 N = int(input('N = '))
