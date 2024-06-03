eca4d3e7512fe3a56916f4eb6451c77b03842933
Saneyuki Okabe <sane@SaneyukinoMacBook-Pro.local>
2024-06-03 15:15:16 +0900

    chapter06

---
+++ okabe/chapter06/knock55.py
@@ -0,0 +1,19 @@
+"""
+55. 混同行列の作成
+52で学習したロジスティック回帰モデルの混同行列（confusion matrix）を，
+学習データおよび評価データ上で作成せよ．
+"""
+
+from knock51 import X_train, X_test, train, test
+from knock52 import lr
+from sklearn.metrics import confusion_matrix
+
+pred_train = lr.predict(X_train)
+pred_test = lr.predict(X_test)
+
+cm_train = confusion_matrix(pred_train, train['CATEGORY'])
+cm_test = confusion_matrix(pred_test, test['CATEGORY'])
+
+if __name__ == '__main__':
+    print(cm_train)
+    print(cm_test)
